89d4c8942639d57ad3beee47d08d70ad1604da8b
Kremlin Huaman Santos <31712084+huamank@users.noreply.github.com>
2024-03-19 05:30:36 +0000

    fix scripts

---
+++ datanode/scripts/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp -R /datanode/sqoop/ /usr/local/
+chmod -R 777 /usr/local/sqoop
---
+++ datanode/scripts/sqoop/script_sqoop.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+sqoop import \
+--connect "jdbc:mysql://172.16.5.4:3310/retail_db" \
+--username=root \
+--password=root \
+--table customers \
+--as-textfile \
+--target-dir=/user/datapath/datasets/customers \
+--delete-target-dir > /tmp/log_customer.log
+
+sqoop import \
+--connect "jdbc:mysql://192.168.220.165:3310/retail_db" \
+--username=root \
+--password=root \
+--table departments \
+--as-textfile \
+--target-dir=/user/datapath/datasets/departments \
+--delete-target-dir > /tmp/log_departments.log
+
+sqoop import \
+--connect "jdbc:mysql://172.16.5.4:3310/retail_db" \
+--username=root \
+--password=root \
+--table categories \
+--as-textfile \
+--target-dir=/user/datapath/datasets/categories \
+--delete-target-dir > /tmp/log_categories.log
+
+sqoop import \
+--connect "jdbc:mysql://172.16.5.4:3310/retail_db" \
+--username=root \
+--password=root \
+--table orders \
+--as-textfile \
+--target-dir=/user/datapath/datasets/orders \
+--delete-target-dir > los_orders.log
+
+sqoop import \
+--connect "jdbc:mysql://172.16.5.4:3310/retail_db" \
+--username=root \
+--password=root \
+--table order_items \
+--as-textfile \
+--target-dir=/user/datapath/datasets/order_items \
+--delete-target-dir > /tmp/log_order_items.log
+
+sqoop import \
+--connect "jdbc:mysql://172.16.5.4:3310/retail_db" \
+--username=root \
+--password=root \
+--table products \
+--as-textfile \
+--target-dir=/user/datapath/datasets/products \
+--delete-target-dir > /tmp/log_products.log
